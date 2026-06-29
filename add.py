740f74f11ed682618ef7d6332be689cdef3971ea
Akash Abhigyan <akashabhigyan@gmail.com>
2026-06-29 13:57:22 +0530

    Fix print statement for sum output in add.py

---
+++ add.py
@@ -0,0 +1,12 @@
+a=5
+b=4.3
+c="Hello"
+d="World"
+print(a+b)
+#print(a+c)/(b+c), not supported
+print(c+" "+d)
+
+e=float(input("Enter a number :"))
+f=float(input("Enter another number :"))
+print("Sum of the numbers is :",e+f)
+
