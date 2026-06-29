a464649dacab0b75d475f9c58175e77b3725bc68
Akash Abhigyan <akashabhigyan@gmail.com>
2026-06-29 18:39:19 +0530

    Initial commit

--- arithmetic.py
+++ arithmetic.py
@@ -1,15 +1,15 @@
-num1=5
-num2=10
-sum=num1+num2
-product=num1*num2
-remainder=num2%num1
-difference=num2-num1
-exponent=num1**num2
-quotient=num2//num1 # Integer division, returns the quotient as an integer
+num1 = 5
+num2 = 10
+sum = num1 + num2
+product = num1 * num2
+remainder = num2 % num1
+difference = num2 - num1
+exponent = num1 ** num2
+quotient = num2 // num1  # Integer division, returns the quotient as an integer
 print(sum)
 print(product)
 print(remainder)
 print(difference)
 print(exponent)
 print(quotient)
-print(num2/num1) # Float division, returns the quotient as a float
+print(num2 / num1)  # Float division, returns the quotient as a float
