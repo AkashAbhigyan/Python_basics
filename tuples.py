b1a18995acd286e0d9830cb0f494f369e4b43512
Akash Abhigyan <akashabhigyan@gmail.com>
2026-07-05 16:10:39 +0530

    tuple

--- tuples.py
+++ tuples.py
@@ -13,3 +13,15 @@ print(add_tuple[0][1])#accessing the second element of the first tuple
 
 name = ("addx","adxacw","accwwc")
 print(tuple+name)#adding two tuples
+
+fruit1=input("Enter first fruit:")
+fruit2=input("Enter second fruit:")
+fruit3=input("Enter third fruit:")
+fruit4=input("Enter forth fruit:")
+fruit5=input("Enter fifth fruit:")
+
+fruits = (fruit1,fruit2,fruit3,fruit4,fruit5)
+print("\nThe fruits are:",fruits)
+print("Total number of fruits are:",len(fruits))
+print("The first fruit:",fruits[0])
+print("The last fruit:",fruits[-1])
