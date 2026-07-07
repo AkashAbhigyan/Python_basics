2a2c6706447240b67611068b603a1fcae7fc269f
Akash Abhigyan <akashabhigyan@gmail.com>
2026-07-07 17:21:08 +0530

    fh

---
+++ filehandling.py
@@ -0,0 +1,25 @@
+file = open('D:\Learning\Github\simple.txt')
+print(file.read())
+file.close()
+
+file = open('D:\Learning\Github\simple.txt', "r")
+print(file.readline())
+file.close()
+
+file = open('D:\Learning\Github\simple.txt', "r")
+print(file.readlines())
+file.close()
+
+file = open('D:\Learning\Github\simple.txt', "w")
+file.write("Python Programming")
+file.close()
+
+file = open('D:\Learning\Github\simple.txt', "a")
+file.write("\nFile Handling")
+file.close()
+
+with open('D:\Learning\Github\simple.txt', "r") as file:
+    print(file.read())
+
+with open('D:\Learning\Github\simple.txt', "w") as file:
+    file.write("Hello Python")
