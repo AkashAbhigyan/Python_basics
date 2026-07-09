3a9a6218e6e90bcc164e35823251696a78f1d7b5
Akash Abhigyan <akashabhigyan@gmail.com>
2026-07-09 21:22:14 +0530

    add

--- OOP.py
+++ OOP.py
@@ -26,8 +26,16 @@ print(dog2.name)
 print(dog2.breed)
 print(dog2.owner.address)
 
-
-
-
-        
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
     
+    def greet(self):
+        print(f"Hello I am {self.name} and I am {self.age} years old")
+
+person1 = Person("Akash","19")
+print("Name:",person1.name)
+print("Age:",person1.age)
+person1.greet()        
+
