afc0ddd3853d685fe06b06ad82bd9e7b78eec6fa
Akash Abhigyan <akashabhigyan@gmail.com>
2026-07-17 13:49:21 +0530

    add

--- OOP/oop17.py
+++ OOP/oop17.py
@@ -1,4 +1,4 @@
-class Car:
+'''class Car:
     def __init__(self,brand,model,year):
         self.brand = brand
         self.model = model
@@ -18,5 +18,54 @@ for vehicle in Vehicles:
     elif isinstance(vehicle,Bike):
         print(f"Type: {type(vehicle).__name__}\nBrand: {vehicle.brand}\nModel: {vehicle.model}\nYear: {vehicle.year}")
     else:
-        print("Invalid vehicle.")
+        print("Invalid vehicle.")'''
        
+
+class Vehicle:
+    def __init__(self,brand,model,year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+
+    def start(self):
+        print("Engine starting")
+
+    def stop(self):
+        print("Engine stopping")
+
+class Car(Vehicle):
+    def __init__(self,brand,model,year,fueltype,no_of_wheels):
+        super().__init__(brand,model,year)
+        self.fueltype = fueltype
+        self.no_of_wheels = no_of_wheels
+
+    def start(self):
+        print("Car starting")
+
+    def stop(self):
+        print("Car stopping")
+
+class Bike(Vehicle):
+    def __init__(self,brand,model,year,fueltype,no_of_wheels):
+        super().__init__(brand,model,year)
+        self.fueltype = fueltype
+        self.no_of_wheels = no_of_wheels
+
+    def start(self):
+        print("Bike starting")
+
+    def stop(self):
+        print("Bike stopping")
+
+Vehicles = [Car("Ford","Mustang",2013,"Diesel",4),Bike("Honda","civic",2015,"Petrol",2)]
+
+for vehicle in Vehicles:
+    if isinstance(vehicle,Car):
+        print(f"Type: {type(vehicle).__name__}\nBrand: {vehicle.brand}\nModel: {vehicle.model}\nYear: {vehicle.year}\nFuel type: {vehicle.fueltype}\nNumber of wheels: {vehicle.no_of_wheels}")
+        vehicle.start()
+        vehicle.stop()
+        print()
+    if isinstance(vehicle,Bike):
+        print(f"Type: {type(vehicle).__name__}\nBrand: {vehicle.brand}\nModel: {vehicle.model}\nYear: {vehicle.year}\nFuel type: {vehicle.fueltype}\nNumber of wheels: {vehicle.no_of_wheels}")
+        vehicle.start()
+        vehicle.stop()
